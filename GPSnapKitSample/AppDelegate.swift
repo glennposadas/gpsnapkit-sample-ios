8736c21b544360b417277ef9f3e277e02fefb3da
Glenn Posadas <admin@glennvon.com>
2018-01-05 12:08:08 +0800

    - Done with setting up views in Call Screen. - Will try to make the Y Distances better of views.

--- GPSnapKitSample/AppDelegate.swift
+++ GPSnapKitSample/AppDelegate.swift
@@ -16,12 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-        self.window = UIWindow(frame: UIScreen.main.bounds)
-        let viewController = ViewController()
-        viewController.view.backgroundColor = .white
-        
-        self.window?.rootViewController = viewController
-        self.window?.makeKeyAndVisible()
+//        self.window = UIWindow(frame: UIScreen.main.bounds)
+//        let viewController = ViewController()
+//        viewController.view.backgroundColor = .white
+//        
+//        self.window?.rootViewController = viewController
+//        self.window?.makeKeyAndVisible()
         
         return true
     }
--- GPSnapKitSample/ViewController.swift
+++ GPSnapKitSample/ViewController.swift
@@ -11,6 +11,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Properties
+    
+    
+    
     // MARK: - Functions
     // MARK: Override
     
@@ -19,64 +23,17 @@ class ViewController: UIViewController {
 
         // Add the views
         
-        let view_Center = UIView()
-        view_Center.backgroundColor = .black
-        self.view.addSubview(view_Center)
+        self.view.addSubview(self.label_YoureOnTheLine)
+        self.label_YoureOnTheLine.snp.makeConstraints { (make) in
+            make.top.equalTo(self.imageView_ProfilePhoto.snp.bottom).offset(30.0)
+            make.leading.trailing.equalToSuperview().inset(30.0)
+            make.height.equalTo(20.0).priority(.high)
+        }
         
-        let view_Top = UIView()
-        view_Top.backgroundColor = .red
-        self.view.addSubview(view_Top)
-        
-        let view_Bottom = UIView()
-        view_Bottom.backgroundColor = .blue
-        self.view.addSubview(view_Bottom)
-        
-        let view_Leading = UIView()
-        view_Leading.backgroundColor = .green
-        self.view.addSubview(view_Leading)
-        
-        let view_Trailing = UIView()
-        view_Trailing.backgroundColor = .gray
-        self.view.addSubview(view_Trailing)
-        
-        // Add constraints to the views
-        
-        view_Center.snp.makeConstraints { (make) in
+        self.view.addSubview(self.label_DoctorName)
+        self.label_YoureOnTheLine.snp.makeConstraints { (make) in
+            make.top.lessThanOrEqualTo(self.label_YoureOnTheLine.snp.bottomMargin)
             make.centerX.equalToSuperview()
-            make.centerY.equalToSuperview()
-            make.height.equalTo(100.0)
-            make.width.equalTo(100.0)
-        }
-        
-        view_Top.snp.makeConstraints { (make) in
-            make.top.equalToSuperview().inset(10.0)
-            make.centerX.equalToSuperview()
-            make.height.equalTo(100.0)
-            make.width.equalTo(100.0)
-        }
-
-        
-        view_Bottom.snp.makeConstraints { (make) in
-            make.top.equalTo(view_Center.snp.bottom).offset(50)
-            make.centerX.equalTo(view_Center.snp.centerX)
-            make.height.equalTo(100.0)
-            make.width.equalTo(100.0)
-        }
-
-        
-        view_Leading.snp.makeConstraints { (make) in
-            make.trailing.equalTo(view_Center.snp.leading).inset(-50)
-            make.centerY.equalTo(view_Center.snp.centerY)
-            make.height.equalTo(100.0)
-            make.width.equalTo(100.0)
-        }
-
-        
-        view_Trailing.snp.makeConstraints { (make) in
-            make.top.equalTo(view_Center.snp.top).inset(50)
-            make.leading.equalTo(view_Center.snp.trailing).offset(50)
-            make.height.equalTo(100.0)
-            make.width.equalTo(100.0)
         }
 
     }
